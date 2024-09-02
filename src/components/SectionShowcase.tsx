15830f6d4d2c7ebf50a38728d773eca35c8c6d24
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-03 01:53:37 +0800

    feat: add SectionShowcase

---
+++ src/components/SectionShowcase.tsx
@@ -0,0 +1,16 @@
+export default function SectionShowcase() {
+  return (
+    <section className="mx-auto h-full max-w-7xl py-12">
+      <div className="flex w-full flex-col items-center gap-10">
+        <h1 className="text-5xl font-extrabold text-[#f5f5f5]">
+          Productivity. Redefined
+        </h1>
+        <p className="max-w-xl text-center text-lg text-[#a7a7a7]">
+          Optimize your efficiency with our cutting-edge IT solution featuring
+          an intuitive dashboard to enhance your workflow
+        </p>
+        <div className="h-[700px] w-full rounded-2xl bg-black"></div>
+      </div>
+    </section>
+  );
+}
