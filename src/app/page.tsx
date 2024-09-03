591b663c0a8c8bde3470c38fa3f02f9dcf26af17
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-04 01:46:45 +0800

    feat: add Footer in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import SectionShowcase from "@/components/SectionShowcase";
 import SectionCapabilities from "@/components/SectionCapabilities";
 import SectionPlans from "@/components/SectionPlans";
 import SectionNewsLetter from "@/components/SectionNewsLetter";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <SectionCapabilities />
       <SectionPlans />
       <SectionNewsLetter />
+      <Footer />
     </main>
   );
 }
