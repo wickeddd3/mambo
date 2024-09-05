64b9f428db2b37d8cb8f0ef89777cc276933daf0
wickeddd3 <pablophilipandrew@gmail.com>
2024-09-06 01:30:00 +0800

    add: class dark in main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={`${inter.variable} ${lato.variable}`}>{children}</body>
     </html>
   );
